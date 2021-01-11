2adcbae11feca2ea8bb9f400aac046e3775d3f03
Ryan Fitzsimmons <ryan.fitzsimmons@outlook.com>
2021-01-11 09:41:25 +0000

    Added read lines module

---
+++ AutomationFramework.Modules/ReadLinesModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AutomationFramework.Modules
+{
+    public class ReadLinesModule<TResult> : Module<TResult> where TResult : ReadLinesResult
+    {
+        public ReadLinesModule(IStageBuilder builder) 
+            : base(builder) { }
+
+        public string FilePath { get; init; }
+        public Encoding Encoding { get; init; } = Encoding.Default;
+
+        protected override TResult DoWork()
+        {
+            var result = Activator.CreateInstance<TResult>();
+            result.Lines = File.ReadLines(FilePath, Encoding).ToArray();
+            return result;
+        }
+    }
+}
